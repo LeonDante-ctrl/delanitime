94e6be3a105feb1e4a62bb11f2e775acf5fc0fc7
dante <maxgamerdu@gmail.com>
2021-11-19 13:32:08 +0300

    added some js to alert out some responses once interacted with

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,5 +37,14 @@ $(".p2").click(function(){
     function () {
     $(this).animate({opacity:'0'});
 })
-
+$('#submission').submit(function(event){
+    var display1 = $('input:first').val();
+    var display2 = $('input#mail').val();
+    if(display1 && display2 != ''){
+        alert ('Thank you loser ' + display1 + ' your subscription is well received and maybe we will get back to you lol');
+    }else {
+        alert ('Invalid input loser, fix that');
+    }
+    event.preventDefault();
+})
 });
