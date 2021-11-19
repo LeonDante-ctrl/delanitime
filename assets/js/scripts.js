33da12b7150adfab4ca0708c2e06e9adc93b7f76
dante <maxgamerdu@gmail.com>
2021-11-19 13:25:58 +0300

    added some javascript code to toggle in and out my pages paragraph elements

---
+++ assets/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $(".image1").click(function(){
+        $(".image1").hide(1200);
+        $(".p1").toggle(1200);
+        $(".p1").show(1200);
+})
+
+$(".p1").click(function(){
+    $(".p1").hide(1200);
+    $(".image1").toggle(1200);
+    $(".image1").show(1200);
+  });
+$(".image2").click(function(){
+    $(".image2").hide(1200);
+    $(".p2").toggle(1200);
+    $(".p2").show(1200);
+  })
+$(".p2").click(function(){
+    $(".p2").hide(1200);
+    $(".image2").toggle(1200);
+    $(".image2").show(1200);
+  });
+
+  $(".image3").click(function(){
+    $(".image3").hide(1200);
+    $(".p3").toggle(1200);
+    $(".p3").show(1200);
+  })
+  $(".p3").click(function(){
+    $(".p3").hide(1200);
+    $(".image3").toggle(1200);
+    $(".image3").show(1200);
+  });  
+
+});
